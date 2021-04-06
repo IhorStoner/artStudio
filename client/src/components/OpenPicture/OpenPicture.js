6f316427acd7dd7e1d68c59fb22670709bd65172
kapepa <kapepayx@gmail.com>
2021-04-06 14:29:16 +0300

    06.04,14:30

--- client/src/components/OpenPicture/OpenPicture.js
+++ client/src/components/OpenPicture/OpenPicture.js
@@ -28,7 +28,49 @@ export default function OpenPicture({ picture }) {
 	}
 
 	const conditions = {
-		"Доставка": "Условия доставки  ".repeat(20),
+		"Доставка": (<div className="open-picture__pay-delivery">
+                  <div className="open-picture__header">
+                      <h5 className="open-picture__h5">Доставка</h5>
+                  </div>
+                  <div className="open-picture__country">
+                      <h6 className="open-picture__h6">Украина</h6>
+                      <span className="open-picture__text">
+                          Доставка заказов осуществляется Транспортной Компанией «Новая Почта». Срок доставки 2-3 дня с момента подтверждения заказа при наличии товара на складе (при отсутствии товара на складе срок доставки может быть увеличен до 6 календарных дней), по Одессе доставка 24 часа с момента подтверждения заказа. Стоимость доставки согласно тарифов ТК «Новая Почта». При заказе товара от 5единиц доставка осуществляется бесплатно.
+                      </span>
+                      <h6 className="open-picture__h6">Другие страны.</h6>
+                      <span className="open-picture__text">
+                          Международная почта – срок доставки 10-16 рабочих дней. Стоимость доставки 15$.
+                      </span>
+                  </div>
+                  <div className="open-picture__pay">
+                      <h5 className="open-picture__h5 open-picture__h5--shifting">
+                          Оплата
+                      </h5>
+                      <h6 className="open-picture__h6">Украина</h6>
+                      <span className="open-picture__text open-picture__text--shift">
+                          Безналичный расчет
+                      </span>
+                      <span className="open-picture__text">
+                          Оплата производится в гривне на карту Приват Банка через приложение Приват24 или терминал с учетом комиссии Вашего Банка.
+                      </span>
+                  </div>
+                  <div className="open-picture__send-pay">
+                      <h5 className="open-picture__h5 open-picture__h5--bold">
+                          Наложенный платеж
+                      </h5>
+                      <span className="open-picture__text">
+                          Оплата при получении в отделении «Нова Почта» + 2% комиссии от суммы заказа за наложенный платеж
+                          Заказы отправляются по предоплате 10% от стоимости заказа, но не менее 100 UAH. Эта сумма является страховочным платежом, который покрывает издержки доставки в обе стороны, если Вы по какой то причине не забрали посылку.
+                          Внимание! Данный способ оплаты (наложенный платеж) является безопасным, но экономически не выгодным, комиссия за наложенный платеж взымается при доставке Новой Почтой. 
+                      </span>
+                  </div>
+                  <div className="open-picture__different-country">
+                      <h6 className="open-picture__h6">Другие страны</h6>
+                      <span className="open-picture__text">
+                          Полная оплата заказа. Оплата любой картой мира.
+                      </span>
+                  </div>
+                </div>),
 		"Оплата": "Способы оплаты  ".repeat(20),
 		"Описание": picture.text
 	}
